a61859deae5fa428170f6c62b389ff93b2099d7c
Raymond Idengeli <ray.idengeli@gmail.com>
2022-12-09 20:25:55 +0100

    chart js Implementation

---
+++ script.js
@@ -0,0 +1,10 @@
+const chart = document.getElementById('chart').getContext('2d');
+
+// new chart instance 
+new chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', ]
+    }
+})
+
